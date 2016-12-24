b24832608450e61d071503629da1e680190ecdbd
Pedro <plazari96@gmail.com>
2016-12-24 00:58:16 -0200

    iniciando o plugin

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Vista Soft API
+ * Description: Plugin criado para exibir todos os imóveis cadastrados pelo usuário!
+ * Version: 1.0.0
+ * Author: Pedro Lazari
+ * Author URI: http://pedrolazari.com
+ */
